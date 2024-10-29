d3f60bff700c8a8e239f36d7686f3e0bcb80c6f1
Olga García <ogartru2003@g.educaand.es>
2024-10-29 10:05:33 +0100

    Version3

--- src/com/google/eclipse/vistas/Hola.java
+++ src/com/google/eclipse/vistas/Hola.java
@@ -3,6 +3,8 @@ package com.google.eclipse.vistas;
 public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!");
+		System.out.println("Holaaaaaa");
+		System.out.println("Hola de nuevo");
 	}
 
 }
---
+++ src/com/google/eclipse/vistas/adios.java
@@ -0,0 +1,7 @@
+package com.google.eclipse.vistas;
+
+public class adios {
+	public static void main(String[] args) {
+		System.out.println("Adios");
+	}
+}
